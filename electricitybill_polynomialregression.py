105d4151a5fdf3f03c032583ad8441cf12f1fa20
csdiyafathima <csdiyafathima@gmail.com>
2026-09-18 09:29:50 +0530

    Create electricitybill_polynomialregression.py

---
+++ electricitybill_polynomialregression.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import joblib
+model = joblib.load("polynomial_regression_model_Electricitybill.pkl")
+st.title("Electricity_Bill_prediction")
+st.write(
+    "Enter theAC Units to predict the Electricity Bill."
+)
+ac_units = st.number_input(
+    " AC (units)",
+    min_value=10.0,
+    max_value=150,
+    value=50.0,
+    step=1.0
+)
+if st.button("Predict Electric Bill"): 
+  if ac_units < 10:
+    st.error("❌ AC Units must be 10 or above.")
+  elif ac_units > 150:
+    st.error("❌ AC Units must not be greater than 150.") 
+  else:
+   new_data = pd.DataFrame({ 
+     "AC_Units": [ac_units] }) 
+   predicted_bill = model.predict(new_data) 
+   st.success( 
+   f"Predicted Electric Bill: ₹{predicted_bill[0]:.2f}" )
